54718dda104cd972c492c620be1fe9f0f8349dfe
SaiTatter <andrei.voicu133@gmail.com>
2026-05-01 15:22:16 +0300

    fix(ui): keep download tooltip out of status bar

--- tests/test_widgets_navigation.py
+++ tests/test_widgets_navigation.py
@@ -9,6 +9,7 @@ from unittest.mock import patch
 from PySide6.QtWidgets import QHeaderView
 
 from ui.library_routes import tracks_album, tracks_artist
+from ui.controllers.top_bar_controller import TopBarController
 from ui.widgets.lrclib_browser_widget import _BrowserPublishDialog
 from ui.widgets.lyrics_editor_widget import LyricsEditorWidget
 from tests.test_support import (
@@ -233,6 +234,25 @@ class LyricsPasteBehaviorTests(unittest.TestCase):
         finally:
             widget.deleteLater()
             app_state.db.close()
+
+    def test_download_missing_tooltip_does_not_populate_status_bar(self):
+        noop = lambda *args, **kwargs: None
+        widget = TopBarController(
+            on_refresh=noop,
+            on_download_missing=noop,
+            on_open_settings=noop,
+            on_open_about=noop,
+            on_toggle_logs=noop,
+            on_toggle_hotkey_hints=noop,
+            on_schedule_search=noop,
+            on_filter_changed=noop,
+        )
+        try:
+            widget.set_download_missing_mode("prefer_synced")
+            self.assertIn("Download missing lyrics", widget.btn_download_missing.toolTip())
+            self.assertEqual(widget.btn_download_missing.statusTip(), "")
+        finally:
+            widget.deleteLater()
 
     def test_open_track_folder_opens_parent_directory(self):
         app_state = simple_app_state()
